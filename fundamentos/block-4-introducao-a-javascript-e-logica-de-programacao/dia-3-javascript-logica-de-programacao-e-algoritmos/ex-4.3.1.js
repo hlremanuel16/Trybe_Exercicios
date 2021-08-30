c4c64a48a9e3cf92d72d9f4811a0022eae71657f
Helder Emanuel Alves de Oliveira <hlremanuel16@gmail.com>
2021-08-30 12:11:36 -0300

    responde ao exercício do dia 4.3

---
+++ fundamentos/block-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/ex-4.3.1.js
@@ -0,0 +1,13 @@
+const n = 5;
+let space = '';
+const column = 5
+
+for (let index = 0; index < column; index += 1) {
+    space += ('*');
+    
+}
+
+for (let index = 0; index < n; index += 1) {
+    console.log(space);
+}
+
---
+++ fundamentos/block-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/ex-4.3.2.js
@@ -0,0 +1,8 @@
+const n = 5;
+let space = '';
+const column = 5
+
+for (let index = 0; index < column; index += 1) {
+    space += ('*');
+    console.log(space);
+}
