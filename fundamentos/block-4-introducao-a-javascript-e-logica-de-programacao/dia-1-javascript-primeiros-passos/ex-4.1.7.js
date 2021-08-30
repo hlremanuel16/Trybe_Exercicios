3f024318daff7640bc3aae894b83fc5891e27836
Helder Emanuel Alves de Oliveira <hlremanuel16@gmail.com>
2021-08-29 23:21:07 -0300

    relocaliza alguns folders e faz os exercicios 10 e 11.

--- fundamentos/block-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/ex-4.1.7.js
+++ fundamentos/block-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/ex-4.1.7.js
@@ -1,2 +1,17 @@
-let score ='90';
+let score ='80';
 
+if (score > 100 || score < 0){
+    console.log('man, are ya crazy?');
+}else if (score >= 90){
+    console.log('A');
+}else if (score >= 80){
+    console.log('B');
+}else if (score >= 70){
+    console.log('C');
+}else if (score >= 60){
+    console.log('D');
+}else if (score <= 50){
+    console.log('F');
+}else{
+    
+}
