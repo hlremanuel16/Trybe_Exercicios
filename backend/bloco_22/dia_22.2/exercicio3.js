fdd18fea70857be12661e17cd1fc39c4ccfda27a
Helder Emanuel Alves de Oliveira <hlremanuel16@gmail.com>
2022-07-14 15:45:49 -0300

    exercícios em atraso

---
+++ backend/bloco_22/dia_22.2/exercicio3.js
@@ -0,0 +1,15 @@
+// const{ firstDegree } = require("./exercicio1")
+
+// async function callFirstDegree(a, b, c) {
+//   try {
+//     await  result = ((a + b) * c);
+//   }
+
+//   firstDegree(1, 3, 2)
+//   .then(result => console.log(`resultado = ${result }`))
+//   .catch(err => console.log(`erro ${err.message}`))
+// }
+
+// module.exports = {
+//   firstDegree
+// };
---
+++ backend/bloco_22/dia_22.2/index.js
@@ -0,0 +1,7 @@
+const{ main } = require("./exercicio4")
+
+// firstDegree(1, 3, 2)
+//   .then(result => console.log(`resultado = ${result }`))
+//   .catch(err => console.log(`erro ${err.message}`))
+
+main()
